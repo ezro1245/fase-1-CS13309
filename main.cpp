c4290f5f313fcae97a383dc47a601e927f379abc
davidharos <dhs.haro@gmail.com>
2021-02-20 17:34:00 -0600

    Updated Comments to clear code readability, defined the part 2 section.

--- main.cpp
+++ main.cpp
@@ -7,32 +7,40 @@ typedef std::chrono::high_resolution_clock Clock;
 
 
 
+
 int main()
 {
-
-    auto t1 = Clock::now();     //Tiempo inicial
+    auto t1 = Clock::now();     //Se incia el reloj del tiempo total de ejecución.
 
     std::ofstream myfile;
 
-    myfile.open ("resultados.txt");
+    myfile.open ("resultados.txt");     //Se abre el archivo para guardar los resultados.
     for (auto& file : std::__fs::filesystem::directory_iterator{"../archivos"}){
 
-        auto t1_ciclo = Clock::now();
+        auto t1_ciclo = Clock::now();   //Se incia el reloj para medir la duración de apertura de archivos.
 
         std::ofstream html_file;
-        html_file.open(file.path().string());
-        html_file.close();
+        html_file.open(file.path().string());  //Se abre el archivo.
 
-        auto t2_ciclo = Clock::now();
+        //********ACTIVIDAD 2************
+
+
+
+        //*******ACTIVIDAD 2**************
+
+
+        html_file.close();//Se cierra el archivo.
+
+        auto t2_ciclo = Clock::now();   //Se termina el reloj para medir la duración de apertura de archivos.
 
         auto time_elapsed_cycle = (std::chrono::duration_cast<std::chrono::microseconds>(t2_ciclo - t1_ciclo).count());
 
         myfile << file.path().string() << " " << time_elapsed_cycle << " microsegundos" << std::endl;
     }
-    myfile.close();
+    myfile.close(); //Se cierra el archivo para guardar los resultados.
 
 
-    auto t2 = Clock::now();     //Tiempo final
+    auto t2 = Clock::now();     //Se deja de medir el tiempo total de ejecución.
 
 
     //Devuelve el tiempo que tardó el algoritmo en milisegundos
